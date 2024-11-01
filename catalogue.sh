643cc5c7c36371bc10b2e19b724725eeeebda098
Your Name <you@example.com>
2024-11-01 15:50:45 +0530

    changes

--- catalogue.sh
+++ catalogue.sh
@@ -1,13 +1,18 @@
 cp mongodb.repo /etc/yum.repos.d/mongo.repo
 cp catalogue.service /etc/systemd/system/catalogue.service
+
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
+
 dnf install nodejs -y
 useradd roboshop
-mkdir /app 
+
+mkdir /app
+
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
 cd /app 
 unzip /tmp/catalogue.zip
+
 cd /app 
 npm install 
 
