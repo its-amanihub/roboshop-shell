75ecc0e2e17e54165988495193a12c4f143a7c25
Your Name <you@example.com>
2024-11-01 16:50:17 +0530

    changes

--- user.sh
+++ user.sh
@@ -2,7 +2,7 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 
 dnf install nodejs -y
-
+cp user.service /etc/systemd/system/user.service
 useradd roboshop
 
 mkdir /app 
@@ -16,5 +16,5 @@ npm install
 
 systemctl daemon-reload
 
-systemctl enable users 
-systemctl restart users
+systemctl enable user 
+systemctl restart user
