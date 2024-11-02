aa8734316790b3a138b955fd0f391205e0bcdb68
Your Name <you@example.com>
2024-11-02 16:40:04 +0530

    modified

--- dispatch.sh
+++ dispatch.sh
@@ -1,27 +1,12 @@
  source common.sh
+ app_name=dispatch
 
 echo -e "$color copy dispatch service file $no_color"
 cp dispatch.service /etc/systemd/system/dispatch.service
 
 echo -e "$color install golang $no_color"
 dnf install golang -y
-
-echo -e "$color add application user $no_color"
-useradd roboshop
-
-echo -e "$color create application directory $no_color"
-mkdir /app 
-
-
-echo -e "$color create application directory $no_color"
-
-curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch-v3.zip 
-cd /app 
-
-
-echo -e "$color extract application content $no_color"
-unzip /tmp/dispatch.zip
-
+app_prerequisites
 
 echo -e "$color download application dependencies $no_color" 
 go mod init dispatch
--- payment.sh
+++ payment.sh
@@ -1,4 +1,5 @@
 source common.sh
+app_name=payment
 
 echo -e "$color copy dispatch service file $no_color"
 cp payment.service /etc/systemd/system/payment.service
@@ -6,19 +7,7 @@ cp payment.service /etc/systemd/system/payment.service
 echo -e "$color install python $no_color"
 dnf install python3 gcc python3-devel -y
 
-echo -e "$color add appliacation user $no_color"
-useradd roboshop
-
-echo -e "$color create a directory $no_color"
-rm -rf /app
-mkdir /app 
-
-echo -e "$color copy download application content $no_color"
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip 
-cd /app 
-
-echo -e "$color extract application content $no_color"
-unzip /tmp/payment.zip
+app_prerequisites
 cd /app 
 
 echo -e "$color download application dependencies $no_color"
