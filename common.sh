a328dff7282092002fefa223a72c162c9a3e96f0
Your Name <you@example.com>
2024-11-02 17:52:44 +0530

    changes

--- common.sh
+++ common.sh
@@ -6,16 +6,21 @@ color= "\e[35m"
     echo -e "$color add application user $no_color"
 useradd roboshop
 
+echo $?
+
 echo -e "$color create application directory $no_color"
 mkdir /app 
 
+echo $?
 
 echo -e "$color create application directory $no_color"
 
 curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip 
+echo $?
 cd /app 
 
 
 echo -e "$color extract application content $no_color"
 unzip /tmp/$app_name.zip
+echo $?
  }
--- dispatch.sh
+++ dispatch.sh
@@ -6,6 +6,9 @@ cp dispatch.service /etc/systemd/system/dispatch.service
 
 echo -e "$color install golang $no_color"
 dnf install golang -y
+
+echo $?
+
 app_prerequisites
 
 echo -e "$color download application dependencies $no_color" 
@@ -13,9 +16,12 @@ go mod init dispatch
 go get 
 go build
 
+echo $?
 
 echo -e "$color start application service $no_color"
 
 systemctl daemon-reload
 systemctl enable dispatch 
 systemctl restart dispatch
+
+echo $?
