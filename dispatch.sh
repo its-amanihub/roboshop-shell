3909d4deede23862ebf6bc57f9e22ff1c1efd08b
Your Name <you@example.com>
2024-11-02 18:11:44 +0530

    chaes

--- dispatch.sh
+++ dispatch.sh
@@ -2,26 +2,26 @@
  app_name=dispatch
 
 echo -e "$color copy dispatch service file $no_color"
-cp dispatch.service /etc/systemd/system/dispatch.service
+cp dispatch.service /etc/systemd/system/dispatch.service &>>$log_file
 
 echo -e "$color install golang $no_color"
-dnf install golang -y
+dnf install golang -y &>>$log_file
 
 echo $?
 
 app_prerequisites
 
 echo -e "$color download application dependencies $no_color" 
-go mod init dispatch
-go get 
-go build
+go mod init dispatch &>>$log_file
+go get &>>$log_file
+go build &>>$log_file
 
 echo $?
 
 echo -e "$color start application service $no_color"
 
-systemctl daemon-reload
-systemctl enable dispatch 
-systemctl restart dispatch
+systemctl daemon-reload &>>$log_file
+systemctl enable dispatch &>>$log_file
+systemctl restart dispatch &>>$log_file
 
 echo $?
